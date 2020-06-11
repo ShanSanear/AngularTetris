0d5a5a11fe1fc6e9eebf84b7010b8c3ad4695610
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-06-11 17:51:56 +0200

    Second commit.

---
+++ ng-tetris/src/app/board.component.ts
@@ -0,0 +1,32 @@
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { COLS, BLOCK_SIZE, ROWS } from './constants';
+
+@Component({
+    selector: 'game-board',
+    templateUrl: 'board.component.html'
+})
+export class BoardComponent implements OnInit {
+    // Get reference to the canvas.
+    @ViewChild('board', { static: true })
+    canvas: ElementRef<HTMLCanvasElement>;
+
+    ctx: CanvasRenderingContext2D;
+    points: number;
+    lines: number;
+    level: number;
+
+    ngOnInit() {
+        this.initBoard();
+    }
+
+    initBoard() {
+        // Get the 2D context that we draw on.
+        this.ctx = this.canvas.nativeElement.getContext('2d');
+
+        // Calculate size of canvas from constants.
+        this.ctx.canvas.width = COLS * BLOCK_SIZE;
+        this.ctx.canvas.height = ROWS * BLOCK_SIZE;
+    }
+
+    play() {}
+}
